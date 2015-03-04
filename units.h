48155eee9636d9c38c8c431784855170b939e07a
Markus <marwes91@gmail.com>
2015-03-04 12:57:54 +0100

    Added binary +, - and *

--- units.h
+++ units.h
@@ -7,10 +7,16 @@ typedef  double distance;
 struct cord
 {
 	distance a[DIM];
+
 	cord & operator +=(cord rhs)
 	{
 		for(int i=0;i<DIM;i++)
 			a[i]+=rhs.a[i];
+	}
+	cord & operator -=(cord rhs)
+	{
+		for(int i=0;i<DIM;i++)
+			a[i]-=rhs.a[i];
 	}
 	cord & operator *=(double rhs)
 	{
@@ -18,4 +24,16 @@ struct cord
 			a[i]*=rhs;
 	}
 };
+inline cord operator+(cord lhs, const cord& rhs) {
+    lhs += rhs;
+    return lhs;
+}
+inline cord operator-(cord lhs, const cord& rhs) {
+    lhs -= rhs;
+    return lhs;
+}
+inline cord operator*(double lhs, cord rhs) {
+    rhs *= lhs;
+    return rhs;
+}
 #endif
