35f41abfe21aec9e01c4f601e6d93da007671a94
Markus <marwes91@gmail.com>
2015-03-12 12:07:35 +0100

    Add pretty printing for body and vec

--- body.cpp
+++ body.cpp
@@ -1,11 +1,15 @@
 #include "body.h"
+#include <iostream>
 
-vec body::forceFrom(const body& other) const
+vec body::forceFrom(const body& other) const {
+    return forceFrom(other.pos, other.m);
+}
+vec body::forceFrom(const vec& other_pos, double other_mass) const
 {
-    vec pos_diff = pos - other.pos;
+    vec pos_diff = pos - other_pos;
     distance sq_dist = pos_diff.sqnorm();
     distance dist = std::sqrt(sq_dist);
-    distance magnitude = G * m * other.m / (sq_dist * dist);
+    distance magnitude = G * m * other_mass / (sq_dist * dist);
     return magnitude * pos_diff;
 }
 void body::update(vec force,double dt )
@@ -15,3 +19,19 @@ void body::update(vec force,double dt )
 	pos+=vel*dt;
 }
 
+std::ostream& operator << (std::ostream& o, const vec& v)
+{
+    o << "(";
+    o << v[0];
+    for (int i = 1; i < DIM; ++i) {
+        o << ", " << v[i];
+    }
+    o << ")";
+    return o; 
+}
+
+std::ostream& operator << (std::ostream& o, const body& b)
+{
+    o << b.m << " " << b.pos << " " << b.vel;
+    return o; 
+}
